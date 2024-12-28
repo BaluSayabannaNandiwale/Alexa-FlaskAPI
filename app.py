41b78faebf88d3a4585d088baea48499ffad1ff6
Balu Nandiwale <162858857+BaluSayabannaNandiwale@users.noreply.github.com>
2024-12-28 10:43:41 +0530

    Add files via upload
    # Alexa Flask API
    
    This project integrates various functionalities like playing songs, telling jokes, fetching weather updates, and more using voice commands. The application is built with Flask and utilizes speech recognition and text-to-speech conversion.
    
    ## Project Overview
    This project uses the following technologies:
    - Flask
    - SpeechRecognition
    - pyttsx3
    - pywhatkit
    - pyjokes
    - Wikipedia API
    - OpenWeatherMap API
    
    ## Features
    - **Play a Song**: Plays a song on YouTube based on the user's command.
    - **Tell a Joke**: Tells a random joke.
    - **Fetch Weather**: Provides the current temperature of a specified city.
    - **Wikipedia Summary**: Gives a brief summary of a person or topic from Wikipedia.
    - **Time**: Tells the current time.
    - **Stop**: Stops the assistant.
    
    ## Installation
    
    1. **Clone the repository**:
        ```sh
        git clone https://github.com/yourusername/alexa-flaskapi.git
        cd alexa-flaskapi
        ```
    
    2. **Create a virtual environment**:
        ```sh
        python -m venv venv
        source venv/bin/activate  # On Windows use `venv\Scripts\activate`
        ```
    
    3. **Install the required packages**:
        ```sh
        pip install -r requirements.txt
        ```
    
    4. **Set your OpenWeatherMap API key**:
        - Replace `<YOUR API KEY>` in the `app.py` file with your actual OpenWeatherMap API key.
    
    ## Running the Application
    
    1. **Set the FLASK_APP environment variable**:
        - On Windows:
            ```sh
            set FLASK_APP=app.py
            ```
        - On MacOS/Linux:
            ```sh
            export FLASK_APP=app.py
            ```
    
    2. **Run the Flask application**:
        ```sh
        flask run
        ```
    
    3. **Open your browser** and go to `http://127.0.0.1:5000/` to see the running application.
    
    ## Project Structure

---
+++ app.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+"""
+@author:    Balu Nanadiwale
+"""
+
+from flask import Flask, render_template, redirect, request
+import warnings
+
+warnings.filterwarnings('ignore')
+
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import pyjokes
+import wikipedia
+import sys
+import requests, json
+
+listener = sr.Recognizer()
+
+app = Flask("__name__")
+
+
+def engine_talk(text):
+    engine = pyttsx3.init()
+    voices = engine.getProperty("voices")
+    engine.setProperty('voice', voices[1].id)
+    engine.say(text)
+    engine.runAndWait()
+
+
+def user_commands():
+    try:
+        with sr.Microphone() as source:
+            print("Start Speaking!!")
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            command = command.replace('alexa', '')
+            if 'alexa' in command:
+                print(command)
+                return command
+    except:
+        pass
+    return ""
+
+
+def weather(city):
+    # Enter your API key here
+    api_key = "<YOUR API KEY>"
+    # How to use api_key, see the below code:
+    # api_key = "ABCDE"
+
+    # base_url variable to store url
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+    # Give city name
+    city_name = city
+
+    # complete_url variable to store
+    # complete url address
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+
+    # get method of requests module
+    # return response object
+    response = requests.get(complete_url)
+
+    # json method of response object
+    # convert json format data into
+    # python format data
+    x = response.json()
+
+    # Now x contains list of nested dictionaries
+    # Check the value of "cod" key is equal to
+    # "404", means city is found otherwise,
+    # city is not found
+    if x["cod"] != "404":
+        # store the value of "main"
+        # key in variable y
+        y = x["main"]
+
+        # store the value corresponding
+        # to the "temp" key of y
+        current_temperature = y["temp"]
+        temp_in_celcius = current_temperature - 273.15
+        return str(int(temp_in_celcius))
+
+
+def run_alexa():
+    command = user_commands()
+    if 'play a song' in command:
+        song = 'Arijit Singh'
+        engine_talk('Playing some music')
+        print("Playing....")
+        pywhatkit.playonyt(song)
+    elif 'play' in command:
+        song = command.replace('play', '')
+        engine_talk('Playing....' + song)
+        print("Playing....")
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        print(time)
+        engine_talk('Current Time is' + time)
+    elif 'joke' in command:
+        get_j = pyjokes.get_joke()
+        print(get_j)
+        engine_talk(get_j)
+    elif 'who is' in command:
+        person = command.replace('who is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        engine_talk(info)
+    elif 'weather' in command:
+        # engine_talk('Please tell name of the city')
+        city = 'Hong Kong'
+        engine_talk('The temperature in Hong Kong is' + weather(city) + ' degree Celsius')
+    elif 'stop' in command:
+        engine_talk("Goodbye")
+    else:
+        engine_talk("I didn't hear you properly")
+        print("I didn't hear you properly")
+
+
+@app.route('/')
+def hello():
+    return render_template("alexa.html")
+
+
+@app.route('/home')
+def home():
+    return redirect('/')
+
+
+@app.route('/run_alexa', methods=['POST', 'GET'])
+def submit():
+    run_alexa()
+    return render_template("alexa.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
